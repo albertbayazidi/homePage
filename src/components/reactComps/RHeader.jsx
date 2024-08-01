b11e339e7b8f9d5f4278cc45ba29189cb0fe62e9
Albert <albert.bayazidi@gmail.com>
2024-08-01 21:16:58 +0200

    go navbar up and running. still missing correct urls
    also need to fix RtoggleTheme should return a button, and must of the cuntonallity should be in its own function

--- src/components/reactComps/RHeader.jsx
+++ src/components/reactComps/RHeader.jsx
@@ -1,14 +1,48 @@
-import RToggleTheme from './RThemeToggle.jsx'
+import { useEffect, useRef, useState } from 'react';
+import RToggleTheme from './RThemeToggle.jsx';
+import siteConfig from '../../site-config.js';
+import getLinkTarget from '../../utils/link.js';
 
+export default function RHeader() {
+  const navBarLogo = siteConfig.header.logo || [];
+  const navLinks = siteConfig.header.navLinks || [];
+  const headerRef = useRef(null);
 
-export default function RHeader(){
-    return(
-    <div>
-		<i id='currTheme' className='i-line-md:moon-rising-alt-loop w-1em h-1em'/>
+  return (
+    <>
+      <header
+        id="header"
+        ref={headerRef}
+        className="w-screen h-20 px-6 flex justify-between items-center relative"
+      >
+        <div className="flex items-center h-full">
+          <a href="/" className="mr-6" aria-label="Header Logo Image">
+            <img width="32" height="32" src={navBarLogo.src} alt={navBarLogo.alt} />
+          </a>
+          <nav className="flex flex-wrap gap-x-6 position-initial flex-row">
+            {navLinks.map((link) => (
+              <a
+                key={link.text}
+                aria-label={link.text}
+                target={link.href.startsWith('http') ? '_blank' : '_self'}
+                className="nav-link"
+                href={link.href}
+              >
+                {link.text}
+              </a>
+            ))}
+          </nav>
+        </div>
+        <div className="meow">/</div>
+        <div className="flex gap-x-6">
+          <a href="https://github.com/albertbayazidi" className="i-ri-github-line" />
+		
 			<button 
-				onClick={(e)=>{RToggleTheme(e)}}
-				>
-				Trykk på meg 	
+				onClick={(e)=>{RToggleTheme(e)}}>
+                        <i id='currTheme' className='i-ri-moon-line'/>
 			</button>
-    </div>)
+        </div>
+      </header>
+    </>
+  );
 }
--- src/components/reactComps/RThemeToggle.jsx
+++ src/components/reactComps/RThemeToggle.jsx
@@ -1,7 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import { isDark } from '../store.js';
 
 export default function RToggleTheme(e) {
-    const endRadius = 1600;
     const x = e.clientX;
     const y = e.clientY;
 
@@ -25,9 +25,10 @@ export default function RToggleTheme(e) {
     transition.ready.then(() => {
         const clipPath = [
             `circle(0px at ${x}px ${y}px)`,
-            `circle(1000px at ${x}px ${y}px)`,
+            `circle(2000px at ${x}px ${y}px)`,
         ];
         console.log('Starting animation with clipPath:', clipPath);
+        console.log(width)
 
         document.documentElement.animate(
             {
@@ -36,7 +37,6 @@ export default function RToggleTheme(e) {
             {
                 duration: 1000,
                 easing: 'ease-in',
-                rangeEnd:"cover 100%",
                 pseudoElement: isDark.value
                     ? '::view-transition-old(root)'
                     : '::view-transition-new(root)',
@@ -46,9 +46,7 @@ export default function RToggleTheme(e) {
         console.error('Error during transition:', error);
     });
 
-    transition.finished.then(() => {
-        console.log('Transition finished');
-    });
+    
 }
 
 function toggleHtmlClass() {
