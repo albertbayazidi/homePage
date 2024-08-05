ee59d8294e3e3d5cef525924b39968fad07b8ccf
Albert <albert.bayazidi@gmail.com>
2024-08-05 21:08:31 +0200

    startet on sgv path for moon

--- src/components/reactComps/RThemeToggle.jsx
+++ src/components/reactComps/RThemeToggle.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { isDark } from '../store.js';
 
 export default function RToggleTheme(e) {
-
     function toggleHtmlClass() {
         const htmlElement = document.documentElement;
         if (isDark.value) {
@@ -11,15 +10,25 @@ export default function RToggleTheme(e) {
             htmlElement.classList.remove('dark');
         }
     }
-    function handleToggle(e){
+
+    function handleToggle(e) {
         const x = e.clientX;
         const y = e.clientY;
+        const endX = 0;
+        const endY = window.innerHeight;
+
+        const startPath = `path('M ${x},${y} A 1 1 0 1 1 ${x} ${2*y} C ${x+20} ${y*1.4} ${x} ${y} ${x} ${y}')`;
+
+
+        const middlePath = `path('M ${x},${y} A 1 1 0 1 1 ${x} ${3*y} C ${x*1.2} ${y*1.8} ${x} ${y} ${x} ${y}')`;
+
+        const endPath = `path('M ${x-100},${y} A 1 1 0 1 1 ${x-100} ${2*y} C ${x-80} ${y*1.4} ${x-100} ${y} ${x-100} ${y}')`;
 
         const element = document.getElementById('currTheme');
         if (element) {
             element.className = !isDark.value 
-                ? 'i-line-md:moon-rising-alt-loop' 
-                : 'i-line-md:moon-filled-alt-to-sunny-filled-loop-transition';
+                ? 'i-line-md:moon-rising-alt-loop rotate-230' 
+                : 'i-line-md:moon-filled-alt-to-sunny-filled-loop-transition rotate-230';
         }
 
         if (!document.startViewTransition) {
@@ -35,10 +44,8 @@ export default function RToggleTheme(e) {
         });
 
         transition.ready.then(() => {
-            const clipPath = [
-                `circle(0px at ${x}px ${y}px)`,
-                `circle(2000px at ${x}px ${y}px)`,
-            ];
+            const clipPath = [startPath, endPath];
+
             console.log('Starting animation with clipPath:', clipPath);
 
             document.documentElement.animate(
@@ -46,7 +53,7 @@ export default function RToggleTheme(e) {
                     clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
                 },
                 {
-                    duration: 1000,
+                    duration: 1500,
                     easing: 'ease-in',
                     pseudoElement: isDark.value
                         ? '::view-transition-old(root)'
@@ -59,10 +66,8 @@ export default function RToggleTheme(e) {
     }
 
     return (
-			<button 
-				onClick={(e)=>{handleToggle(e)}}>
-                <i id='currTheme' className='i-ri-moon-line'/>
-			</button>
-    ) 
+        <button onClick={(e) => { handleToggle(e) }}>
+            <i id='currTheme' className='i-ri-moon-line rotate-230' />
+        </button>
+    )
 }
-
