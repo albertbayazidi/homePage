0e2c774792e752ca799339f47672229aaaaf2836
albert bayazidi <albert.bayazidi@gmail.com>
2024-08-08 14:46:06 +0200

    hovereffect on links in projects, should rem to add link to pages

--- src/pages/Projects/data.js
+++ src/pages/Projects/data.js
@@ -1,20 +1,21 @@
 export const projects = [
   {
-    title: 'Homemade Ir Camera',
+    title: 'DIY IR-Camera',
     description: 'LMAO',
     icon: 'i-hugeicons:fire',
     href: '/',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 16 16"><path fill="currentColor" d="M9.32 15.653a.81.81 0 0 1-.086-.855c.176-.342.245-.733.2-1.118a2.1 2.1 0 0 0-.267-.779a2 2 0 0 0-.541-.606a3.96 3.96 0 0 1-1.481-2.282c-1.708 2.239-1.053 3.51-.235 4.63a.75.75 0 0 1-.014.901a.87.87 0 0 1-.394.283a.84.84 0 0 1-.478.023c-1.105-.27-2.145-.784-2.85-1.603a4.7 4.7 0 0 1-.906-1.555a4.8 4.8 0 0 1-.263-1.797s-.133-2.463 2.837-4.876c0 0 3.51-2.978 2.292-5.18a.62.62 0 0 1 .112-.653a.56.56 0 0 1 .623-.147l.146.058a7.63 7.63 0 0 1 2.96 3.5c.58 1.413.576 3.06.184 4.527c.325-.292.596-.641.801-1.033l.029-.064c.198-.477.821-.325 1.055-.013c.086.137 2.292 3.343 1.107 6.048a5.5 5.5 0 0 1-1.84 2.027a6.1 6.1 0 0 1-2.138.893a.83.83 0 0 1-.472-.038a.87.87 0 0 1-.381-.29zM7.554 7.892a.42.42 0 0 1 .55.146q.06.09.075.198l.045.349c.02.511.014 1.045.213 1.536c.206.504.526.95.932 1.298a3.06 3.06 0 0 1 1.16 1.422c.22.564.25 1.19.084 1.773a4.1 4.1 0 0 0 1.39-.757l.103-.084c.336-.277.613-.623.813-1.017c.201-.393.322-.825.354-1.269c.065-1.025-.284-2.054-.827-2.972c-.248.36-.59.639-.985.804c-.247.105-.509.17-.776.19a.8.8 0 0 1-.439-.1a.83.83 0 0 1-.321-.328a.83.83 0 0 1-.035-.729c.412-.972.54-2.05.365-3.097a5.87 5.87 0 0 0-1.642-3.16c-.156 2.205-2.417 4.258-2.881 4.7a4 4 0 0 1-.224.194c-2.426 1.965-2.26 3.755-2.26 3.834a3.7 3.7 0 0 0 .459 2.043c.365.645.89 1.177 1.52 1.54C4.5 12.808 4.5 10.89 7.183 8.14l.372-.25z"/></svg>',
   },
   {
     title: 'asC',
     description: 'LMAO',
-    icon: 'i-line-md:cog-loop',
     href: '/',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 24 24"><defs><symbol id="lineMdCogLoop0"><path fill="none" stroke-width="2" d="M15.24 6.37C15.65 6.6 16.04 6.88 16.38 7.2C16.6 7.4 16.8 7.61 16.99 7.83C17.46 8.4 17.85 9.05 18.11 9.77C18.2 10.03 18.28 10.31 18.35 10.59C18.45 11.04 18.5 11.52 18.5 12"><animate fill="freeze" attributeName="d" begin="1.2s" dur="0.3s" values="M15.24 6.37C15.65 6.6 16.04 6.88 16.38 7.2C16.6 7.4 16.8 7.61 16.99 7.83C17.46 8.4 17.85 9.05 18.11 9.77C18.2 10.03 18.28 10.31 18.35 10.59C18.45 11.04 18.5 11.52 18.5 12;M15.24 6.37C15.65 6.6 16.04 6.88 16.38 7.2C16.38 7.2 19 6.12 19.01 6.14C19.01 6.14 20.57 8.84 20.57 8.84C20.58 8.87 18.35 10.59 18.35 10.59C18.45 11.04 18.5 11.52 18.5 12"/></path></symbol></defs><g fill="none" stroke="currentColor" stroke-width="2"><g stroke-linecap="round" stroke-linejoin="round"><path stroke-dasharray="42" stroke-dashoffset="42" d="M12 5.5C15.59 5.5 18.5 8.41 18.5 12C18.5 15.59 15.59 18.5 12 18.5C8.41 18.5 5.5 15.59 5.5 12C5.5 8.41 8.41 5.5 12 5.5z" opacity="0"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.3s" dur="0.75s" values="42;0"/><set attributeName="opacity" begin="0.3s" to="1"/><set attributeName="opacity" begin="1.05s" to="0"/></path><path stroke-dasharray="20" stroke-dashoffset="20" d="M12 9C13.66 9 15 10.34 15 12C15 13.66 13.66 15 12 15C10.34 15 9 13.66 9 12C9 10.34 10.34 9 12 9z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="20;0"/></path></g><g opacity="0"><use href="#lineMdCogLoop0"/><use href="#lineMdCogLoop0" transform="rotate(60 12 12)"/><use href="#lineMdCogLoop0" transform="rotate(120 12 12)"/><use href="#lineMdCogLoop0" transform="rotate(180 12 12)"/><use href="#lineMdCogLoop0" transform="rotate(240 12 12)"/><use href="#lineMdCogLoop0" transform="rotate(300 12 12)"/><set attributeName="opacity" begin="1.05s" to="1"/><animateTransform attributeName="transform" dur="45s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></g></g></svg>',
   },
   {
     title: 'Protein Folder',
     description: 'LMAO',
-    icon: 'i-fluent:molecule-16-regular',
     href: '/',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 16 16"><path fill="currentColor" d="M11 8a3 3 0 1 0-2.707-1.705l-2.34 1.17a2.5 2.5 0 1 0 .76 3.2l2.296 1.148a2 2 0 1 0 .343-.946l-2.359-1.18Q7 9.596 7 9.5c0-.463-.126-.897-.345-1.268L8.878 7.12A3 3 0 0 0 11 8m0-1a2 2 0 1 1 0-4a2 2 0 0 1 0 4m-6.5 4a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3m7.5 1a1 1 0 1 1-2 0a1 1 0 0 1 2 0"/></svg>',
   },
 ];
