f392229223d7af5a368c47ed1079d32dc8e9b585
albert <albert.bayazidi@gmail.com>
2026-02-18 11:01:27 +0100

    upgraded to newst astro

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/Projects" }),
+  
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(), 
+    svg: z.string(),
+    coolFactor: z.number(),
+  })
+});
+
+export const collections = { 
+  Projects: projects 
+};
